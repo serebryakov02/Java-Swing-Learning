18daf76d653ba0773a1dcf31f416393817876da5
serebryakov02 <nik04ta01@gmail.com>
2023-07-22 16:25:00 +0200

    jdbc
    added connecting to a SQL db, retrieving values from a db and saving to a db functionality

---
+++ src/TestDatabase.java
@@ -0,0 +1,30 @@
+import java.sql.SQLException;
+
+import model.AgeCategory;
+import model.Database;
+import model.EmploymentCategory;
+import model.Gender;
+import model.Person;
+
+public class TestDatabase {
+	public static void main(String[] args) {
+		System.out.println("Running database test");
+		
+		Database db = new Database();
+		db.connect();
+		
+		db.addPerson(new Person("Mykyta", "C++/Qt Developer", AgeCategory.adult, 
+				EmploymentCategory.selfEmployed, "4444", true, Gender.male));
+		db.addPerson(new Person("Olya", "English teacher", AgeCategory.adult, 
+				EmploymentCategory.employed, null, false, Gender.female));
+		
+		try {
+			db.save();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		db.disconnect();
+	}
+}
--- src/model/Database.java
+++ src/model/Database.java
@@ -6,6 +6,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -14,8 +19,93 @@ import java.util.List;
 public class Database {
 	private List<Person> people;
 	
+	private Connection connection;
+	private String jdbcUrl = "jdbc:mysql://localhost:3306/persons";
+	private String username = "root";
+	private String password = "Dk8vf4m5";
+	
 	public Database() {
 		people = new LinkedList<Person>();
+	}
+	
+	public void connect() {
+		 if (connection == null) {
+		        try {
+		            // Load the MySQL JDBC driver
+		            Class.forName("com.mysql.cj.jdbc.Driver");
+
+		            // Establish the database connection
+		            connection = DriverManager.getConnection(jdbcUrl, username, password);
+		            System.out.println("Connected to MySQL database successfully.");
+		        } catch (ClassNotFoundException e) {
+		            System.err.println("MySQL JDBC driver not found.");
+		            e.printStackTrace();
+		        } catch (SQLException e) {
+		            System.err.println("Error while connecting to MySQL database.");
+		            e.printStackTrace();
+		        }
+		    } else {
+		        System.out.println("Already connected to the database.");
+		    }
+	}
+	
+	public void disconnect() {
+		try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                System.out.println("Disconnected from MySQL database.");
+            }
+        } catch (SQLException e) {
+            System.err.println("Error while disconnecting from MySQL database.");
+            e.printStackTrace();
+        }
+	}
+	
+	public void save() throws SQLException {
+		String checkSql = "select count(*) as count from person where id =?";
+		PreparedStatement checkStmt = connection.prepareStatement(checkSql);
+		
+		String insertSql = "insert into person(id, name, age, employment_status, "
+				+ "tax_id, us_citizen, gender, occupation) values(?, ?, ?, ?, ?, ?, ?, ?)";
+		PreparedStatement insertStmt = connection.prepareStatement(insertSql);
+		
+		for(Person person : people) {
+			int id = person.getId();
+			String name = person.getName();
+			String occupation = person.getOccupation();
+			AgeCategory ageCat = person.getAgeCategory();
+			EmploymentCategory empCat = person.getEmpCat();
+			String taxID = person.getTaxID();
+			boolean isUsCitizen = person.isUsCitizen();
+			Gender gender = person.getGender();
+			
+			checkStmt.setInt(1, id);
+			
+			ResultSet checkResult = checkStmt.executeQuery();
+			checkResult.next();
+			
+			int count = checkResult.getInt(1);
+			if (count == 0) {
+				System.out.println("Inserting person with ID " + id);
+				
+				int col = 1;
+				insertStmt.setInt(col++, id);
+				insertStmt.setString(col++, name);
+				insertStmt.setString(col++, ageCat.name());
+				insertStmt.setString(col++, empCat.name());
+				insertStmt.setString(col++, taxID);
+				insertStmt.setBoolean(col++, isUsCitizen);
+				insertStmt.setString(col++, gender.name());
+				insertStmt.setString(col++, occupation);
+				
+				insertStmt.executeUpdate();
+			} else {
+				System.out.println("Updating person with ID " + id);
+			}
+		}
+		
+		insertStmt.close();
+		checkStmt.close();
 	}
 	
 	public void addPerson(Person person) {
@@ -50,7 +140,6 @@ public class Database {
 			people.clear();
 			people.addAll(Arrays.asList(persons));
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		ois.close();
