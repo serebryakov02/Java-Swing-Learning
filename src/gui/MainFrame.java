9f127cc5960568bf17f2d560f0bfc13e937b9758
serebryakov02 <nik04ta01@gmail.com>
2023-07-24 10:33:40 +0200

    added icons, toolbar and intercepting the window closing event func

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -5,6 +5,9 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.prefs.Preferences;
@@ -36,8 +39,20 @@ public class MainFrame extends JFrame {
 
 		setMinimumSize(new Dimension(500, 400));
 		setSize(800, 600);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		setVisible(true);
+		
+		addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				controller.disconnect();
+				dispose();
+				// garbage collector
+				System.gc();
+			}
+			
+		});
 
 		setLayout(new BorderLayout());
 
@@ -230,7 +245,13 @@ public class MainFrame extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				int action = JOptionPane.showConfirmDialog(MainFrame.this, "Do you really want to exit the app?",
 						"Confirm Exit", JOptionPane.OK_CANCEL_OPTION);
-				System.exit(0);
+				
+				if (action == JOptionPane.OK_OPTION) {
+					WindowListener[] listeners = getWindowListeners();
+					for (WindowListener listener : listeners) {
+						listener.windowClosing(new WindowEvent(MainFrame.this, 0));
+					}
+				}
 			}
 		});
 
--- src/gui/Toolbar.java
+++ src/gui/Toolbar.java
@@ -1,28 +1,42 @@
 package gui;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.net.URL;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.JToolBar;
 
-public class Toolbar extends JPanel {
+public class Toolbar extends JToolBar {
 	private JButton saveBtn;
 	private JButton refreshBtn;
 	private ToolbarListener textListener;
 	
 	public Toolbar() {
 		setBorder(BorderFactory.createEtchedBorder());
+		setFloatable(false);
 		
-		saveBtn = new JButton("Save");
-		refreshBtn = new JButton("Refresh");
+		saveBtn = new JButton();
+		saveBtn.setIcon(createIcon("/images/floppy-disk.png"));
+		saveBtn.setToolTipText("Save");
 		
-		setLayout(new FlowLayout(FlowLayout.LEFT));
+		refreshBtn = new JButton();
+		refreshBtn.setIcon(createIcon("/images/refresh.png"));
+		refreshBtn.setToolTipText("Refresh");
+		
 		add(saveBtn);
 		add(refreshBtn);
+	}
+	
+	private ImageIcon createIcon(String path) {
+		URL url = getClass().getResource(path);
+		if (url == null) {
+			System.err.println("Unable to load image: " + path);
+		}
+		
+		ImageIcon icon = new ImageIcon(url);
+		return icon;
 	}
 	
 	public void setToolbarListener(ToolbarListener listener) {
